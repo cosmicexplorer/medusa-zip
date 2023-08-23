0239f03d54edef2f59899e1dc2349b0bbd89963c
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-08-23 15:29:35 -0400

    add example criterion benchmark perf comparison

--- lib/benches/my_benchmark.rs
+++ lib/benches/my_benchmark.rs
@@ -7,20 +7,46 @@
  * Licensed under the Apache License, Version 2.0 (see LICENSE).
  */
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
-#[inline]
-fn fibonacci(n: u64) -> u64 {
+fn fibonacci_slow(n: u64) -> u64 {
   match n {
     0 => 1,
     1 => 1,
-    n => fibonacci(n - 1) + fibonacci(n - 2),
+    n => fibonacci_slow(n - 1) + fibonacci_slow(n - 2),
   }
 }
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-  c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+fn fibonacci_fast(n: u64) -> u64 {
+  let mut a = 0;
+  let mut b = 1;
+
+  match n {
+    0 => b,
+    _ => {
+      for _ in 0..n {
+        let c = a + b;
+        a = b;
+        b = c;
+      }
+      b
+    },
+  }
 }
 
-criterion_group!(benches, criterion_benchmark);
+
+fn bench_fibs(c: &mut Criterion) {
+  let mut group = c.benchmark_group("Fibonacci");
+  for i in [20u64, 21u64].iter() {
+    group.bench_with_input(BenchmarkId::new("Recursive", i), i, |b, i| {
+      b.iter(|| fibonacci_slow(*i))
+    });
+    group.bench_with_input(BenchmarkId::new("Iterative", i), i, |b, i| {
+      b.iter(|| fibonacci_fast(*i))
+    });
+  }
+  group.finish();
+}
+
+criterion_group!(benches, bench_fibs);
 criterion_main!(benches);
