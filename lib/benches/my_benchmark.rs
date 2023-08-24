0c517122dbfa3a9fd1e6a04da6f2dd14480f708a
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-08-23 22:12:18 -0400

    split up knobs for parameterized benchmarks

--- lib/benches/my_benchmark.rs
+++ lib/benches/my_benchmark.rs
@@ -7,7 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (see LICENSE).
  */
 
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use criterion::{
+  criterion_group, criterion_main, BenchmarkId, Criterion, SamplingMode, Throughput,
+};
 
 mod parallel_merge {
   use super::*;
@@ -80,18 +82,32 @@ mod parallel_merge {
        * some small true changes. */
       .significance_level(0.01);
 
-    for (filename, n, t) in [
+    for (filename, (n_p, n_sync), (t_p, t_sync), (noise_p, noise_sync), sampling_mode) in [
       (
         /* This file is 37K. */
         "Keras-2.4.3-py2.py3-none-any.whl",
-        500,
-        Duration::from_secs(7),
+        (500, 500),
+        (Duration::from_secs(7), Duration::from_secs(7)),
+        /* This says we don't care about changes under 15%, which is a huge gap,
+         * but otherwise the synchronous benchmarks will constantly signal spurious changes. */
+        (0.03, 0.15),
+        SamplingMode::Auto,
       ),
       (
         /* This file is 1.2M. */
         "Pygments-2.16.1-py3-none-any.whl",
-        100,
-        Duration::from_secs(24),
+        (100, 100),
+        (Duration::from_secs(8), Duration::from_secs(24)),
+        (0.1, 0.2),
+        SamplingMode::Auto,
+      ),
+      (
+        /* This file is 9.7M. */
+        "Babel-2.12.1-py3-none-any.whl",
+        (80, 10),
+        (Duration::from_secs(24), Duration::from_secs(35)),
+        (0.2, 0.3),
+        SamplingMode::Flat,
       ),
       /* ( */
       /*   /\* This file is 461M, or about half a gigabyte, with multiple individual very */
@@ -111,30 +127,35 @@ mod parallel_merge {
       let id = format!("{}({} bytes)", filename, zip_len);
 
       group
-        .sample_size(*n)
-        .measurement_time(*t)
-        .throughput(Throughput::Bytes(zip_len as u64));
+        .throughput(Throughput::Bytes(zip_len as u64))
+        .sampling_mode(*sampling_mode);
 
       /* FIXME: assigning `_` to the second arg of this tuple will destroy the
        * extract dir, which is only a silent error producing an empty file!!!
        * AWFUL UX!!! */
       let (input_files, _tmp_extract_dir) = extract_example_zip(&target).unwrap();
 
-      for (noise_threshold, parallelism) in [
-        (0.03, lib::zip::Parallelism::ParallelMerge),
-        /* This says we don't care about improvements under 5%, which is a huge gap,
-         * but otherwise the synchronous keras benchmark will report e.g. 4.8%
-         * improvement immediately after the last bench. */
-        (0.05, lib::zip::Parallelism::Synchronous),
-      ]
-      .iter()
-      {
-        group.noise_threshold(*noise_threshold);
-        group.bench_with_input(BenchmarkId::new(&id, parallelism), parallelism, |b, p| {
-          b.to_async(&rt)
-            .iter(|| create_basic_zip(input_files.clone(), *p));
-        });
-      }
+      /* Run the parallel implementation. */
+      let parallelism = lib::zip::Parallelism::ParallelMerge;
+      group
+        .sample_size(*n_p)
+        .measurement_time(*t_p)
+        .noise_threshold(*noise_p);
+      group.bench_with_input(BenchmarkId::new(&id, parallelism), &parallelism, |b, p| {
+        b.to_async(&rt)
+          .iter(|| create_basic_zip(input_files.clone(), *p));
+      });
+
+      /* Run the sync implementation. */
+      let parallelism = lib::zip::Parallelism::Synchronous;
+      group
+        .sample_size(*n_sync)
+        .measurement_time(*t_sync)
+        .noise_threshold(*noise_sync);
+      group.bench_with_input(BenchmarkId::new(&id, parallelism), &parallelism, |b, p| {
+        b.to_async(&rt)
+          .iter(|| create_basic_zip(input_files.clone(), *p));
+      });
     }
 
     group.finish();
